c2edc69a4caecdc3cc648d017fee9c54336e3a3a
Ian <ian.nasoore@student.moringaschool.com>
2025-12-08 06:31:48 +0300

    bookings done

--- config/__init__.py
+++ config/__init__.py
@@ -1,6 +1,10 @@
 """
 Configuration package for Find My Chef
 """
-from config.settings import Config, config
+from config.settings import Settings, settings
+
+# Exporting names expected elsewhere: aliasing Settings->Config and settings->config
+Config = Settings  # Providing backwards-compatible name 'Config'
+config = settings  # Providing backwards-compatible name 'config'
 
 __all__ = ['Config', 'config']
